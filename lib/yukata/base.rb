3752b3c34740ce145363ec2651becda1c1024908
Matthew Johnston <warmwaffles@gmail.com>
2014-03-04 23:46:49 -0600

    Added some options to #attribute
    Hooked into attr_accessor to register the attribute with the list of
    known attributes.
    
    Allow for attributes to optionally allow writers and readers.
    
    Allow for attributes to skip the coercion and just leave the object as
    is.

--- lib/yukata/base.rb
+++ lib/yukata/base.rb
@@ -22,7 +22,7 @@ module Yukata
     def attributes
       hash = {}
       self.class.attributes.keys.each do |k|
-        hash[k] = self.send(k)
+        hash[k] = send(k) if respond_to?(k)
       end
       hash
     end
@@ -35,30 +35,51 @@ module Yukata
       @attributes ||= {}
     end
 
+    # @override
+    def self.attr_accessor(*args)
+      args.each { |name| self.attributes[name] = Attribute.new(Object) }
+      super
+    end
+
     # Declares an attribute on the model
     #
     # @param name [String]
     # @param type [Class] a class that represents the type
     # @param options [Hash] extra options to apply to the attribute
     def self.attribute(name, type=String, options={})
+      config = { writer: true, reader: true, coerce: true }.merge(options)
+
       attr = Attribute.new(type, options)
       self.attributes[name] = attr
       variable = "@#{name}"
 
-      define_method("#{name}=") do |object|
-        val = Yukata.coercer.coerce(object, attr.type)
-        instance_variable_set(variable, val)
-      end
-
-      define_method(name) do
-        val = instance_variable_get(variable)
-        unless val
-          val = attr.default
-          instance_variable_set(variable, val)
+      # -------------
+      # Define Writer
+      # -------------
+      if config[:writer]
+        define_method("#{name}=") do |object|
+          if config[:coerce]
+            val = Yukata.coercer.coerce(object, attr.type)
+            instance_variable_set(variable, val)
+          else
+            instance_variable_set(variable, object)
+          end
         end
-        val
       end
 
+      # -------------
+      # Define reader
+      # -------------
+      if config[:reader]
+        define_method(name) do
+          val = instance_variable_get(variable)
+          unless val
+            val = attr.default
+            instance_variable_set(variable, val)
+          end
+          val
+        end
+      end
     end
   end
 end
--- spec/base_spec.rb
+++ spec/base_spec.rb
@@ -2,29 +2,70 @@ require 'spec_helper'
 
 describe Yukata::Base do
   class MockedBase < Yukata::Base
+    attr_accessor :qux
     attribute :id,         Integer
     attribute :foo,        String
-    attribute :created_at, DateTime, default: -> { DateTime.now }
+    attribute :created_at, Date, default: -> { Date.today }
   end
+
+  let(:today) { Date.today }
+  let(:params) { { id: '1', foo: 'bar', created_at: today, qux: 'doh' } }
 
   describe '#initialize' do
     context 'when a hash is not passed' do
       subject { MockedBase.new }
 
-      its(:id) { should be_nil }
-      its(:foo) { should be_nil }
-      its(:created_at) { should_not be_nil }
+      it 'sets id to nil' do
+        expect(subject.id).to be_nil
+      end
+
+      it 'sets foo to nil' do
+        expect(subject.foo).to be_nil
+      end
+
+      it 'sets qux to nil' do
+        expect(subject.qux).to be_nil
+      end
     end
 
     context 'when a hash is passed' do
-      let(:now) { DateTime.now }
-      let(:params) { { id: '1', foo: 'bar', created_at: now } }
-
       subject { MockedBase.new(params) }
 
-      its(:id) { should eq(1) }
-      its(:foo) { should eq('bar') }
-      its(:created_at) { should eq(now) }
+      it 'sets id to 1' do
+        expect(subject.id).to eq(1)
+      end
+
+      it 'sets foo to "bar"' do
+        expect(subject.foo).to eq('bar')
+      end
+
+      it 'sets qux to "doh"' do
+        expect(subject.qux).to eq('doh')
+      end
+
+      it 'sets created_at to today' do
+        expect(subject.created_at).to eq(today)
+      end
+    end
+  end
+
+  describe '#attributes' do
+    subject { MockedBase.new(params).attributes }
+
+    it 'contains the :id' do
+      expect(subject[:id]).to eq(1)
+    end
+
+    it 'contains the :foo' do
+      expect(subject[:foo]).to eq('bar')
+    end
+
+    it 'contains the :qux' do
+      expect(subject[:qux]).to eq('doh')
+    end
+
+    it 'contains the :created_at' do
+      expect(subject[:created_at]).to eq(today)
     end
   end
 end
